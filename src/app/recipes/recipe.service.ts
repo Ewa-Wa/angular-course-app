fdf4ad0b10dd8e1d911d37c26f5df35849e8c8d6
Ewa-Wa <ewa261611@gmail.com>
2020-12-04 22:49:04 +0100

    http

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,24 +8,8 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-  new Recipe(
-  'Pumpkin cream',
-  'Very creamy, delicious, vegan mniam',
-  'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg',
-  [
-    new Ingredient('pumpkin', 1),
-    new Ingredient('cream', 2)
-  ]),
-  new Recipe(
-  'Steak',
-  'Simply raw steak',
-  'https://www.thespruceeats.com/thmb/kxu3tr3tIob-VCMNTKc5b7g2VFA=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/marinated-top-round-steak-3060302-hero-02-ed071d5d7e584bea82857112aa734a94.jpg',
-  [
-      new Ingredient('steak', 1),
-      new Ingredient('butter', 1)
-  ])
-  ];
+private recipes: Recipe[] = [];
+
 
   constructor(private slService: ShoppingListService){}
 
@@ -53,6 +37,11 @@ export class RecipeService {
 
   deleteRecipe(index: number) {
     this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
     this.recipesChanged.next(this.recipes.slice());
   }
 }
